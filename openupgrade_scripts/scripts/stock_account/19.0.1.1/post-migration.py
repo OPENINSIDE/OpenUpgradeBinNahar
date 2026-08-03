6263bc8fc4f7a44f4d43ae82ce0a26ba382f1183
Zuhair Hammadi <zuhair.hammadi@open-inside.com>
2026-08-03 13:20:43 +0300

    Implement parallel execution for SQL updates in post-migration script

--- openupgrade_scripts/scripts/stock_account/19.0.1.1/post-migration.py
+++ openupgrade_scripts/scripts/stock_account/19.0.1.1/post-migration.py
@@ -3,19 +3,36 @@
 
 from openupgradelib import openupgrade
 
+from odoo.upgrade import util
+import logging
+import datetime
+from odoo.sql_db import db_connect
+_logger = logging.getLogger(__name__)
+
+def explode_execute(cr, query, *args, **kwargs):
+    _logger.info("Execute a query in parallel: %s", query)
+    cr.commit()  # Commit the current transaction before executing in parallel
+    with db_connect(cr.dbname).cursor() as cr2:
+        start_time = datetime.datetime.now()
+        util.explode_execute(cr2, query, *args, **kwargs)
+        end_time = datetime.datetime.now()
+        _logger.info("Query executed in parallel in %s", (end_time - start_time))
+
 
 def product_value_product_id(env):
     """
     Fill product.value#product_id from move_id.product_id
     """
-    env.cr.execute(
+    explode_execute(
+        env.cr,
         """
         UPDATE product_value
         SET product_id=stock_move.product_id
         FROM stock_move
         WHERE product_value.move_id=stock_move.id
         AND product_value.product_id IS NULL
-        """
+        """,
+        table="product_value",
     )
 
 
@@ -23,7 +40,8 @@ def stock_move_account_move_id(env):
     """
     Fill stock.move#account_move_id from account.move#stock_move_id
     """
-    env.cr.execute(
+    explode_execute(
+        env.cr,
         """
         UPDATE stock_move
         SET account_move_id=account_move.id
@@ -31,7 +49,8 @@ def stock_move_account_move_id(env):
         WHERE
         account_move.stock_move_id=stock_move.id
         AND stock_move.account_move_id IS NULL
-        """
+        """,
+        table="stock_move",
     )
 
 
@@ -78,7 +97,8 @@ def stock_move_value(env):
     """
     Set stock.move#value to sum of product.value#value for this move
     """
-    env.cr.execute(
+    explode_execute(
+        env.cr,
         """
         UPDATE stock_move
         SET value=aggregated_values.value
@@ -90,7 +110,8 @@ def stock_move_value(env):
             GROUP BY move_id
         ) aggregated_values
         WHERE aggregated_values.move_id=stock_move.id
-        """
+        """,
+        table="stock_move",
     )
 
 
