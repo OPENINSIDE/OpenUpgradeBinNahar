398c3304f0f97a3144ebe82243e9a1ac3cd0cc72
Zuhair Hammadi <zuhair.hammadi@open-inside.com>
2026-08-27 15:56:37 +0300

    [OU-ADD] Refactor explode_execute function for parallel query execution in post-migration and pre-migration scripts

--- openupgrade_scripts/scripts/crm/19.0.1.9/post-migration.py
+++ openupgrade_scripts/scripts/crm/19.0.1.9/post-migration.py
@@ -2,10 +2,25 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from openupgradelib import openupgrade
 
+from odoo.upgrade import util
+import logging
+import datetime
+from odoo.sql_db import db_connect
+_logger = logging.getLogger(__name__)
+
+def explode_execute(cr, query, *args, **kwargs):
+    _logger.info("Execute a query in parallel: %s", query)
+    cr.commit()  # Commit the current transaction before executing in parallel
+    with db_connect(cr.dbname).cursor() as cr2:
+        start_time = datetime.datetime.now()
+        util.explode_execute(cr2, query, *args, **kwargs)
+        end_time = datetime.datetime.now()
+        _logger.info("Query executed in parallel in %s", (end_time - start_time))
+
 
 def _migrate_lead_mobile(env):
     openupgrade.copy_columns(env.cr, {"crm_lead": [("phone", None, None)]})
-    openupgrade.logged_query(
+    explode_execute(
         env.cr,
         """
         UPDATE crm_lead
@@ -17,6 +32,7 @@ def _migrate_lead_mobile(env):
         WHERE COALESCE(mobile, '') != ''
         AND COALESCE(phone, '') != COALESCE(mobile, '')
         """,
+        table="crm_lead",
     )
 
 
--- openupgrade_scripts/scripts/crm/19.0.1.9/pre-migration.py
+++ openupgrade_scripts/scripts/crm/19.0.1.9/pre-migration.py
@@ -2,6 +2,21 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from openupgradelib import openupgrade
 
+from odoo.upgrade import util
+import logging
+import datetime
+from odoo.sql_db import db_connect
+_logger = logging.getLogger(__name__)
+
+def explode_execute(cr, query, *args, **kwargs):
+    _logger.info("Execute a query in parallel: %s", query)
+    cr.commit()  # Commit the current transaction before executing in parallel
+    with db_connect(cr.dbname).cursor() as cr2:
+        start_time = datetime.datetime.now()
+        util.explode_execute(cr2, query, *args, **kwargs)
+        end_time = datetime.datetime.now()
+        _logger.info("Query executed in parallel in %s", (end_time - start_time))
+
 
 def _won_status_set(env):
     # Precreate won_status and set with sql
@@ -11,7 +26,7 @@ def _won_status_set(env):
             ("crm.lead", "won_status", "selection", "pending", "crm_lead"),
         ],
     )
-    openupgrade.logged_query(
+    explode_execute(
         env.cr,
         """
         UPDATE crm_lead cl
@@ -21,8 +36,10 @@ def _won_status_set(env):
         AND cs.is_won
         AND cl.probability = 100
         """,
+        table="crm_lead",
+        alias="cl"
     )
-    openupgrade.logged_query(
+    explode_execute(
         env.cr,
         """
         UPDATE crm_lead cl
@@ -30,6 +47,8 @@ def _won_status_set(env):
         WHERE NOT cl.active
         AND cl.probability = 0
         """,
+        table="crm_lead",
+        alias="cl"
     )
 
 
